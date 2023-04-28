02bcdc24af3ca419a5421ee34dc70878ce585d99
pierresprim <wincopies@wincopies.com>
2023-04-28 05:15:08 +0200

    Update InstallerPage.cs

--- Source/Shared/Common/WinCopies.Installer.Shared/Models/ObjectModel/Pages/InstallerPage.cs
+++ Source/Shared/Common/WinCopies.Installer.Shared/Models/ObjectModel/Pages/InstallerPage.cs
@@ -1,4 +1,22 @@
-﻿using System.Windows.Media;
+﻿/* Copyright © Pierre Sprimont, 2022
+ *
+ * This file is part of the WinCopies Framework.
+ *
+ * The WinCopies Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The WinCopies Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the WinCopies Framework.  If not, see <https://www.gnu.org/licenses/>. */
+
+using System.Drawing;
+using System.Windows.Media;
 
 namespace WinCopies.Installer
 {
@@ -37,7 +55,9 @@ namespace WinCopies.Installer
 
         public abstract string Description { get; }
 
-        public virtual string NextStepName => "Next";
+        public abstract Icon Icon { get; }
+
+        public virtual string NextStepName => "_Next";
 
         public abstract ImageSource ImageSource { get; }
 
@@ -62,7 +82,9 @@ namespace WinCopies.Installer
         {
             get
             {
-                if (_nextPageLoaded) return _nextPage;
+                if (_nextPageLoaded) 
+                    
+                    return _nextPage;
 
                 _nextPageLoaded = true;
 
